398d7f94aa5dc8242724a0880c1f62a4fbbebf20
Valentin <valentin.lucet@gmail.com>
2022-10-08 18:38:04 -0400

    further test - trying

--- tests/test_mdtools.py
+++ tests/test_mdtools.py
@@ -1,4 +1,10 @@
 from mdtools import __version__
+from mdtools import convert
 
 def test_version():
     assert __version__ == '0.1.0'
+    
+def test_md_to_coco_ct():
+    convert.md_to_coco_ct("tests/test_images/test_images_output.json", 
+                          "tests/test_images/test_images_output_coco_ct.json", 
+                          "tests/test_images")
