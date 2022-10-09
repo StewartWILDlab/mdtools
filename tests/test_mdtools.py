2c51691f86c7cd4616969da2e3064b6c9a56c3de
Valentin <valentin.lucet@gmail.com>
2022-10-08 22:21:47 -0400

    fix tests

--- tests/test_mdtools.py
+++ tests/test_mdtools.py
@@ -1,10 +1,11 @@
 from mdtools import __version__
-from mdtools import convert
+from mdtools.convert import *
 
 def test_version():
     assert __version__ == '0.1.0'
     
 def test_md_to_coco_ct():
-    convert.md_to_coco_ct("tests/test_images/test_images_output.json", 
-                          "tests/test_images/test_images_output_coco_ct.json", 
-                          "tests/test_images")
+    ret = md_to_ls("tests/test_images/test_images_output.json",  "tests/test_images", 
+                   output_json_coco = "tests/test_images/test_images_output_coco_ct.json", 
+                   output_json_ls="tests/test_images/test_images_output_ls.json")
+    assert isinstance(ret, dict)
