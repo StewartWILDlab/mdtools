10080164bdd6c83e95f30741a1c3b305314fe511
Valentin <valentin.lucet@gmail.com>
2022-10-08 22:32:49 -0400

    fix tests

--- tests/test_mdtools.py
+++ tests/test_mdtools.py
@@ -4,4 +4,5 @@ def test_md_to_coco_ct():
     ret = md_to_ls("tests/test_images/test_images_output.json",  "tests/test_images", 
                    output_json_coco = "tests/test_images/test_images_output_coco_ct.json", 
                    output_json_ls="tests/test_images/test_images_output_ls.json")
-    assert isinstance(ret, dict)
+    assert isinstance(ret, list)
+    assert isinstance(ret[0], dict)
