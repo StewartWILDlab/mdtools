8a423ae9d18d1b4779d633519ca0b2860d033aa0
Valentin <valentin.lucet@gmail.com>
2022-10-21 15:13:55 -0400

    orks

--- mdtools/mdtools.py
+++ mdtools/mdtools.py
@@ -3,18 +3,20 @@ import click
 from mdtools import convert as mdc
 
 
-@click.group()
+@click.group
 def mdtools():
     pass
 
 
-@mdtools.command()
-@click.argument("md_json", 
+@mdtools.command('convert')
+@click.argument("out",
+                type=click.Choice(['cct', 'ls', 'csv'], case_sensitive=False))
+@click.argument("md_json",
                 type=click.Path(exists=True))
-@click.argument("conf_threshold", 
+@click.argument("conf_threshold",
                 type=click.FLOAT)
 @click.option("-bd", "--image-base-dir",
-              help="Base directory of images", 
+              help="Base directory of images",
               default=".")
 @click.option("-ru", "--image-root-url",
               help="", default="/data/local-files/?d=")
@@ -26,13 +28,20 @@ def mdtools():
               help="", default=None)
 @click.option("-ol", "--output-json-ls",
               help="", default=None)
-def convert(md_json, conf_threshold, image_base_dir, image_root_url, 
-            write_coco, output_json_coco, write_ls, output_json_ls):
-    mdc.md_to_ls(click.format_filename(md_json),
-                 conf_threshold,
-                 image_base_dir,
-                 image_root_url,
-                 write_coco,
-                 output_json_coco,
-                 write_ls,
-                 output_json_ls)
+def convert(out, md_json, conf_threshold, image_base_dir, image_root_url,
+            write_coco, output_json_coco, write_ls, output_json_ls):    
+            
+    if (out == "cct" | format == "ls"):
+            
+        mdc.md_to_ls(click.format_filename(md_json),
+                    conf_threshold,
+                    image_base_dir,
+                    image_root_url,
+                    write_coco,
+                    output_json_coco,
+                    write_ls,
+                    output_json_ls)
+        
+    elif (out == "csv"):
+        
+        mdc.md_to_csv("md_json")
