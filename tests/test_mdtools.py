6877f572c31c29bbdd4d67cbe8dcbd6244010c9c
Valentin <valentin.lucet@gmail.com>
2022-10-31 10:29:09 -0400

    fix first test

--- tests/test_mdtools.py
+++ tests/test_mdtools.py
@@ -1,8 +1,10 @@
 from mdtools.convert import *
-    
-def test_md_to_coco_ct():
-    ret = md_to_ls("tests/test_images/test_images_output.json",  "tests/test_images", 
-                   output_json_coco = "tests/test_images/test_images_output_coco_ct.json", 
+
+
+def test_md_to_ls():
+    ret = md_to_ls("tests/test_images/test_images_output.json", 0.1, "tests/test_images",
+                   write_coco = True, write_ls = True,
+                   output_json_coco="tests/test_images/test_images_output_coco_ct.json",
                    output_json_ls="tests/test_images/test_images_output_ls.json")
     assert isinstance(ret, list)
     assert isinstance(ret[0], dict)
