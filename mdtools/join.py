f1a7c751445f407e47773a7c09c581906be5ffc4
Valentin <valentin.lucet@gmail.com>
2022-11-06 13:26:34 -0500

    add join

--- mdtools/join.py
+++ mdtools/join.py
@@ -1,4 +1,13 @@
 import pandas as pd
 
-def join_exif_to_csv(csv_file, exif_file):
-    pass
+
+def join_exif_to_csv(csv_file, exif_file, join_file, by="SourceFile"):
+    csv = pd.read_csv(csv_file)
+    csv["SourceFile"] = ("/media/vlucet/TrailCamST/TrailCamStorage/" 
+                         + csv["folder"] + "/" + csv["file"])
+    exif=pd.tead_csv(exif_file)
+
+    joined=pd.merge(csv, exif, 
+                    how = "left", on="SourceFile")
+    
+    joined.to_csv(join_file)
