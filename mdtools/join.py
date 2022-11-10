a4fc341a8a36fcbc2e1284a8cda9600b6a4b5c10
Valentin <valentin.lucet@gmail.com>
2022-11-10 10:37:44 -0500

    simplify join

--- mdtools/join.py
+++ mdtools/join.py
@@ -1,15 +1,16 @@
 import pandas as pd
 
 
-def join_exif_to_csv(csv_file, exif_file, join_file, write=True, by="SourceFile"):
+def join_exif_to_csv(csv_file, exif_file, join_file, write=True, by="source_file"):
     csv = pd.read_csv(csv_file)
-    csv["SourceFile"] = (
-        "/media/vlucet/TrailCamST/TrailCamStorage/" + csv["folder"] + "/" + csv["file"]
-    )
+    # csv["SourceFile"] = (
+    #     "/media/vlucet/TrailCamST/TrailCamStorage/" + csv["folder"] + "/" + csv["file"]
+    # )
     exif = pd.read_csv(exif_file)
 
     joined = pd.merge(csv, exif, how="left", on=by)
+    
     if write:
-        joined.to_csv(join_file)
+        joined.to_csv(join_file, index = False)
 
     return joined
