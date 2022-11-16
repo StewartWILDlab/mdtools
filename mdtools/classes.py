6a1f15ee2b48244064ce663f0fc0cf7d509d2d0d
Valentin <valentin.lucet@gmail.com>
2022-11-16 12:04:35 -0500

    revamp classes once again

--- mdtools/classes.py
+++ mdtools/classes.py
@@ -5,106 +5,107 @@ import json
 
 
 class MDResult:
-    """Megadetector result class (wrap around a json)."""
+    """Megadetector result class."""
 
-    def __init__(self, root: str, folder: str, file: str):
+    def __init__(self, root: str, folder: str, md_file: str):
         """Initialize the class."""        
         # Check if dir
         if not os.path.isdir(root):
             raise Exception("Result root must be a directory.")
 
-        # Property
-        self._defaultcategories = {'1': 'animal', '2': 'person', '3': 'vehicle'}
-
         # Provided
         self.root: str = root
         self.folder: str = folder
-        self.file: str = file
+        self.md_file: str = md_file
+        
+        # Computed
+        self.md_filepath: str = os.path.join(self.root, self.md_file)
+        
+        # Properties
+        self._default_md_categories = {'1': 'animal', '2': 'person', '3': 'vehicle'}
+        # self._format: str="MD"
 
         # Created
-        self.filepath: str = os.path.join(self.root, self.file)
-        with open(self.filepath, "r") as f:
-            data = json.loads(f.read()) 
-        self.data: dict = data
-        
-        # Future placeholders
-        self.annotations: dict = {}
-        self.predictions: dict = {}
+        with open(self.md_filepath, "r") as f:
+            md_data = json.loads(f.read()) 
+        self.md_data: dict = md_data
 
         # Check categories sanity
-        self.check_categories()
+        self.check_md_categories()
 
     # Property methods
     @property
-    def defaultcategories(self) -> dict:
+    def default_md_categories(self) -> dict:
         """Property default categories."""
-        return(self._defaultcategories)
+        return(self._default_md_categories)
+
+    # @property
+    # def __format__(self) -> str:
+    #     """Property default categories."""
+    #     return(self._format)
 
     # Utils methods
-    def check_categories(self) -> None:
+    def check_md_categories(self) -> None:
         """Check whether the categories are valid."""
-        if not self.categories() == self._defaultcategories:
+        if not self.md_categories() == self._default_md_categories:
             raise Exception("Categories do not match megadetector defaults")
 
     def __repr__(self) -> str:
         """Represent the class."""
-        return f"MD Result with root '{self.root}', folder '{self.folder}', and file '{self.file}' "
+        return ("MD Results in MD format: \n" +
+        f"  * MD file @ '{self.md_filepath}'")
 
-    def images(self) -> dict:
+    # Data methods
+    def md_images(self) -> dict:
         """Get images dict."""
-        return self.data["images"]
+        return self.md_data["images"]
 
-    def numimages(self) -> int:
+    def md_numimages(self) -> int:
         """Compute number of images in the result."""
-        return len(self.images())
+        return len(self.md_images())
 
-    def categories(self) -> dict:
+    def md_categories(self) -> dict:
         """Get categories dict."""
-        return self.data["detection_categories"]
+        return self.md_data["detection_categories"]
 
-    def info(self) -> dict:
+    def md_info(self) -> dict:
         """Get info dict."""
-        return self.data["info"]
-
-    # Write methods
-    def write(self, format: str):
-        """Write data out."""
-        # TODO: check attributes, write if possible
-        pass
-
-    # Plot methods?
-    # TODO
+        return self.md_data["info"]
 
 
-# class CTSurvey:
-#     """Megadetector survey class (wrap around a list of jsons)."""
+class COCOResult(MDResult):
+    """Megadetector coco class (wrap around a json)."""
 
-#     def __init__(self, root, results=[]):
-#         """Initialize the class."""
-#         # Check if nor dir
-#         if not os.path.isdir(root):
-#             raise Exception("Survey root must be a directory.")
+    def __init__(self, root: str, folder: str, md_file: str, 
+        coco_filepath: str, coco_data: dict={}):
+        """Initialize the class."""
+        super().__init__(root, folder, md_file)        
+        
+        # Provided
+        self.coco_file: str = os.path.basename(coco_filepath)
+        self.coco_data: dict = coco_data
 
-#         self.root = root
-#         self.results = results
+        # Computed
+        self.coco_filepath: str = coco_filepath
 
-#     def __repr__(self) -> str:
-#         """Represent the class."""
-#         return f"CT Survey with root '{self.root}' and {len(self.results)} results"
+        # TODO: if empty, maybe load from file?
 
-#     def load_from_folder_names(self, pattern):
-#         """Load all the folders in a survey based on a file pattern."""
-#         folders_paths = [f.path for f in os.scandir(self.root) if f.is_dir()]
-#         results_paths = [(f + pattern) for f in folders_paths if os.path.isfile(f + pattern)]
+        # # Property
+        # self._format: str="JSON"
 
-#         if len(results_paths) == 0:
-#             raise Exception("Not result files found.")
-#         else:
-#             results_files = [os.path.basename(f_path) for f_path in results_paths]
-#             self.results = list(
-#                 map(
-#                     lambda x, y: MDResult(self.root, os.path.basename(x), y),
-#                     folders_paths,
-#                     results_files,
-#                 )
-#             )
+    # Utils methods
+    def __repr__(self) -> str:
+        """Represent the class."""
+        rep =  ("MD Results in COCO format: \n" +
+        f" * MD file   @ '{self.md_filepath}' \n" +
+        f" * COCO file @ '{self.coco_filepath}'")
+        return rep
+
+    # Data methods
+    def annotations(self) -> dict:
+        """Get annotations dict."""
+        return self.coco_data["annotations"]
+
+    def numannotations(self) -> int:
+        """Compute number of images in the result."""
+        return len(self.annotations())
