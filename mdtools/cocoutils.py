6f17ad1e4e52340ee347c3af734374a6f233874a
Valentin <valentin.lucet@gmail.com>
2022-11-16 13:12:21 -0500

    fix pep for this file

--- mdtools/cocoutils.py
+++ mdtools/cocoutils.py
@@ -4,16 +4,19 @@ from datetime import datetime
 
 from mdtools.classes import MDResult
 
-def create_coco_info_dict(md_result: MDResult, version: float=1.0) -> dict:
-	"""Create the info directory for COCO data."""
-	info = {}
-	
-	md_time = md_result.md_info()["detection_completion_time"]
-	info["year"] = datetime.strptime(md_time, '%Y-%m-%d %H:%M:%S').year
-	
-	info["version"] = version
-	
-	md_version = md_result.md_info()["detector_metadata"]["megadetector_version"]
-	info["description"] = f"COCO json generated from MegaDetector {md_version} Results"
 
-	return info
+def create_coco_info_dict(md_result: MDResult, version: float = 1.0) -> dict:
+    """Create the info directory for COCO data."""
+    coco_info = {}
+    md_info = md_result.md_info()
+
+    md_time = md_info["detection_completion_time"]
+    coco_info["year"] = datetime.strptime(md_time, '%Y-%m-%d %H:%M:%S').year
+
+    coco_info["version"] = version
+
+    md_version = md_info["detector_metadata"]["megadetector_version"]
+    coco_info["description"] = ("COCO json generated from MegaDetector " +
+                                f"{md_version} Results")
+
+    return coco_info
