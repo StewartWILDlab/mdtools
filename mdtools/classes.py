2726cb1963932ff7bb2551fb091e128d990078b4
vlucet <valentin.lucet@gmail.com>
2023-10-24 15:13:28 -0400

    test

--- mdtools/classes.py
+++ mdtools/classes.py
@@ -58,7 +58,7 @@ class MDResult:
             name = out_dir + "_output_coco_norepeats.json"
         else:
             name = out_dir + "_output_coco.json"
-        print("test: "  +  output_folder)
+        print("test: "  +  name)
         return name
 
     def make_ls_write_path(self, output_folder:str, repeat: bool) -> str:
